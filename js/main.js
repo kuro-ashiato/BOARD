fb0c109be63cf7ff0a5c7cd7032280c6457ae9a5
kuro-ashiato <developerashiato@gmail.com>
2023-09-05 17:16:47 +0900

    new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const pageBtnEls = document.querySelectorAll(".center-num .pagebtn");
 
+pageBtnEls[0].classList.add("clicked");
+
 pageBtnEls.forEach(function (pageBtnEl) {
     pageBtnEl.addEventListener("click", function (event) {
         if (pageBtnEl.classList.contains("clicked")) {
