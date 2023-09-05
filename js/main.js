e5f503bd119fd441aeb075788ffd32b6ae67fe92
kuro-ashiato <developerashiato@gmail.com>
2023-09-05 14:24:09 +0900

    new

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// const itemNameEl = document.querySelector(".item__name");
+// const subMenuEl = document.querySelector(".item__contents");
+
+// itemNameEl.addEventListener("mouseover", function (event) {
+//     subMenuEl.classList.add("show");
+
+// });
+
+// itemNameEl.addEventListener("mouseleave", function (event) {
+//     subMenuEl.classList.remove("show");
+// });
